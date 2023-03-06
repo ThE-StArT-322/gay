9c07ba68ae0f828e9c2b1a8d78e9cd0bd9df8f45
ThE-StArT-322 <126772957+ThE-StArT-322@users.noreply.github.com>
2023-03-06 11:32:30 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,79 @@
+// 'use strict'
+
+//  let  numberOfFilms;
+
+//  function start() {
+//      numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели', '');
+
+//      while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+//          numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели', '');
+//      }
+//  }
+
+//  start();
+
+//  const personalMovieDB = {
+//      count: numberOfFilms,
+//      movies: {},
+//      actors: {},
+//      genres: [],  
+//      privat: false
+//  };
+
+
+//  function writeYourGenres() {
+//      for (let i = 1; i <= 3; i++) { 
+//          personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`).trim();
+
+        
+//      }
+
+//  }   
+
+//  writeYourGenres();
+
+
+//  function showMyDB() {
+//      if (personalMovieDB.privat === false)
+//          console.log('done');
+//          else {
+//              console.log('error');
+//          }
+//  }
+
+//  showMyDB();
+
+//  function remeberMyFilms() {
+//      for (let i = 0; i < 2; i++) {
+//          const a = prompt('Один из последних просмотренных фильмов?', '').trim(),
+//                b = prompt('На сколько оцените его?', '');
+        
+//          if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//              personalMovieDB.movies[a] = b;
+//              console.log('done');
+//          } else {
+//              console.log('error');
+//             i--;
+//          }
+    
+        
+//      }
+//  }
+
+//  remeberMyFilms();
+//  function detectPersonalLevel() {
+//      if (personalMovieDB.count < 10) {
+//          console.log('Просмотрено довольно мало фильмов');
+//      } else if (personalMovieDB.count >= 10 || personalMovieDB < 30) {
+//          console.log('Вы классический зритель');
+//      } else if (personalMovieDB.count >= 30) {
+//          console.log('Вы киноман');
+//      } else {
+//          console.log('Произошла ошибка');
+//      }
+//  }
+
+//  detectPersonalLevel();
+
+//  console.log(personalMovieDB);
+
